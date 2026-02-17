e8dd46f56b4832e895f102c121f6dc599ff505ce
kaiaiaiii <kaihrig2012@gmail.com>
2026-02-17 19:00:00 +0100

    Erster GPIO read

--- Programmierung/DMS/DMS.cpp
+++ Programmierung/DMS/DMS.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <wiringPi.h>
 using namespace std;
-
+//soweit ich das richtig interpretiere gibt es auser libgpiod nichts wirkliches, und das ist auch seit 3 Jahren tod
 int main(){
+    if(wiringPiSetup()== -1){
+        return 1;
+        }
+    pinMode(0, OUTPUT);
+    while(1){
+        digitalWrite(0, 1); 
+        delay(100); 
+        digitalWrite(0, 0); 
+        delay(100);
+    }
     return 0;
 }
