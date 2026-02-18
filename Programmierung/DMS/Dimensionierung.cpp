7a98a98152de8c27c5d6e3b4cd58f0a042c909fd
kaiaiaiii <kaihrig2012@gmail.com>
2026-02-18 23:30:05 +0100

    erste Rechnungen auch in cpp

--- Programmierung/DMS/Dimensionierung.cpp
+++ Programmierung/DMS/Dimensionierung.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <cmath> 
 using namespace std;
 //Hier werden die Dimensionierungsrechnungen gemacht
-float Ausschlagsspannung;
-float Empfindlichkeit;
+double Ausschlagsspannung;
+double Empfindlichkeit;
+double U0 = 5.00; //Volt, input vom raspi
 
-float Ausschlag(int R1, int R_DMS, int R3, int R_4, int U0){
-    Ausschlagsspannung = 
+
+double Ausschlag(double R1, int R_DMS, double R3, double R4){
+    Ausschlagsspannung = U0*((R3 / (R3+R4))-(R1 / (R1+R_DMS)));
     return Ausschlagsspannung;
 }
 
-float Empfindlichkeit(int R1, int R_DMS, int U0){
-    Empfindlichkeit = U0 * ((R1)/((R1+R_DMS)*(R1+R_DMS)));
-    cout << Empfindlichkeit;
+double Empfindlichkeit_Funktion(int R1, int R_DMS){
+    Empfindlichkeit = U0 * (R1 / pow((R1 + R_DMS), 2));
     return Empfindlichkeit;
 }
 
 int main(){
-    
+    double Wert = Empfindlichkeit_Funktion(350,350);
+    for (int i = 340; i<360; i++){
+        double Value = Ausschlag(350,i,350,350);
+        cout << Value;
+    }
     return 0;
 }
