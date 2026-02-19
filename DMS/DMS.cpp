d42885185635ecce6d8bafda5f5dcde6ccd6f56f
kaiaiaiii <kaihrig2012@gmail.com>
2026-02-19 10:15:23 +0100

    Auswertung gestartet, jetzt wird erstmal viel Hardware gemacht

--- DMS/DMS.cpp
+++ DMS/DMS.cpp
@@ -2,21 +2,20 @@
 #include <wiringPi.h>
 using namespace std;
 //soweit ich das richtig interpretiere gibt es auser libgpiod nichts wirkliches, und das ist auch seit 3 Jahren tod
+double Value_PIN8;
+double Value_PIN10;
 
-vector<string> WriteDataToFile(string Filename){
+
+vector<string> WriteDataToFile(string Filename, double U1/*=Pin8*/, double U2/*=Pin10*/){
     
 }
 
 int main(){
-    if(wiringPiSetup()== -1){
-        return 1;
-        }
-    pinMode(0, OUTPUT);
-    while(1){
-        digitalWrite(0, 1); 
-        delay(100); 
-        digitalWrite(0, 0); 
-        delay(100);
+    while(true){
+    double Value_PIN8 = digitalRead(8);
+    double Value_PIN10 = digitalRead(10);
+    WriteDataToFile(Value_PIN10, Value_PIN8);
+
     }
     return 0;
 }
