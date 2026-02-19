fb1c6383a9fb150748dbe8ffb6c41a5e7b0ed716
kaiaiaiii <kaihrig2012@gmail.com>
2026-02-19 14:33:11 +0100

    Auswertung klappt, Berechnungen auch, Random Datensatz zum testen

--- DMS/Auswertung.py
+++ DMS/Auswertung.py
@@ -1,12 +1,47 @@
 import numpy as np
 import pandas as pd
-import matplolib.pyplot as plt
+import matplotlib.pyplot as plt
 
+### VORGEGEBENE WERTE ###
+U_0 = 5 
+R = 350
+k = 2.0 #evtl auch 2.2
+
+### AUSWERTUNG ###
 data = pd.read_csv('DMS_Daten.txt', sep='\s+',header=None)
 data = pd.DataFrame(data)
+Zeit = data[0]
+U_B = data[1]
+delta_R = []
+Errechnete_Dehnung = []
+
+def del_R(U_B, U_0, R):
+    return (((2*U_B)/U_0)*(2*R))/(1-((2*U_B)/U_0))
+
+def Dehnung(delta_R, R, k):
+    return delta_R/(R*k) 
+
+for i in range(len(Zeit)):
+    delta_R.append(del_R(U_B[i], U_0, R))
+    Errechnete_Dehnung.append(Dehnung(delta_R[i], R, k))
+
+Max_Dehnung = max(Errechnete_Dehnung)
+Max_Dehnung_Zeit = Errechnete_Dehnung.index(Max_Dehnung)
 
 
-x = data[0]
-y = data[1]
+plt.figure(figsize=(8, 5))
+plt.plot(Zeit, U_B, color='green', label="Brueckenspannung")
+plt.plot(Max_Dehnung_Zeit, Max_Dehnung, color='yellow', marker='o', label="Brueckenspannung Maximum")
+plt.plot(Zeit, delta_R, color='red', label="delta_R")
+plt.plot(Zeit, Errechnete_Dehnung, color='orange', label="Errechnete_Dehnung")
+plt.xlabel("Zeit")
+plt.ylabel("Dehnung")
+plt.title("Dehnung ueber Zeit")
+plt.xticks(range(0, len(Zeit), 10), Zeit[::10], rotation = 90)
+plt.legend()
+plt.show()
+
+
+
 
 
--- DMS/Dimensionierung.cpp
+++ DMS/Dimensionierung.cpp
@@ -2,26 +2,34 @@
 #include <cmath> 
 using namespace std;
 //Hier werden die Dimensionierungsrechnungen gemacht
-double Ausschlagsspannung;
+double Brueckenspannung;
+double Brueckenspannung_Strom;
 double Empfindlichkeit;
-double U0 = 5.00; //Volt, input vom raspi
+double U_0 = 5.00; //Volt, input vom raspi
+double I_0 = 5; // Ampere, evtl auch input
 // double Empfindlichkeit_Datenblatt=2.0;
 // double Dehnlimit_Datenblatt = 0.02;
 
-double Ausschlag(double R1, int R_DMS, double R3, double R4){
-    Ausschlagsspannung = U0*((R3 / (R3+R4))-(R1 / (R1+R_DMS)));
-    return Ausschlagsspannung;
+double Brueckenspannung_Spannungsgespeist(double delta_R, double R_134){      // nach Elektrische Messtechnik Seite 131
+    Brueckenspannung = U_0*((delta_R)/(2*(2*R_134+delta_R)));
+    return Brueckenspannung;
+}
+
+double Brueckenspannung_Stromgespeist(double delta_R, double R_134){      // nach Elektrische Messtechnik Seite 131
+    Brueckenspannung_Strom = I_0*((delta_R)/(2*(2*R_134+delta_R)));
+    return Brueckenspannung_Strom;
 }
 
 double Empfindlichkeit_Funktion(int R1, int R_DMS){
-    Empfindlichkeit = U0 * (R1 / pow((R1 + R_DMS), 2));
+    Empfindlichkeit = U_0 * (R1 / pow((R1 + R_DMS), 2));
     return Empfindlichkeit;
 }
 
 int main(){
     double Wert = Empfindlichkeit_Funktion(350,350);
-    for (int i = 340; i<360; i++){
-        double Value = Ausschlag(350,i,350,350);
+    for (int i = 1; i<20; i++){
+        double laufvariable = i/100.0;
+        double Value = Brueckenspannung_Spannungsgespeist(laufvariable,350);
         cout << Value << endl;
     }
     return 0;
