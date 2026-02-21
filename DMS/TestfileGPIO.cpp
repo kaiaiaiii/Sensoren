34a739370c3a3270873c246ccb6e797da3e73422
kaiaiaiii <kaihrig2012@gmail.com>
2026-02-21 21:29:01 +0100

    erste GPIO Tests starten

---
+++ DMS/TestfileGPIO.cpp
@@ -0,0 +1,15 @@
+#include <wiringPi.h> 
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  wiringPiSetupGpio();
+
+  pinMode(17, INPUT);
+  pullUpDnControl(17, PUD_DOWN);
+
+  int value = digitalRead(17);
+
+  cout << value;
+}
