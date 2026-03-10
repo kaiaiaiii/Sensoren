597e592f583fb27d9427170c38fda943662ae7e9
kaiaiaiii <kaihrig2012@gmail.com>
2026-03-10 20:37:14 +0100

    Testfile GPIO

--- GPIOTesting/Testfile.cpp
+++ GPIOTesting/Testfile.cpp
@@ -6,6 +6,13 @@
 #define PIN 15
 
 int main() {
+    // wiringPi initialisieren
+    if (wiringPiSetup() == -1) {
+        std::cerr << "Fehler beim Initialisieren von wiringPi!" << std::endl;
+        return 1;
+    }
+
+    // Pin als Eingang setzen
     pinMode(PIN, INPUT);
 
     std::cout << "Lese Werte von GPIO Pin 15..." << std::endl;
